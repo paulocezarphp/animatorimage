5578a2b07df2a0b374b0e7b3144e6f7fac61a178
Paulo <cezarleigos28@outlook.com>
2021-09-03 09:47:25 -0300

    v0.02 -  Creating dashboard
    This time we start creating the user panel, we will create two parts inside the panel, an area that shows the object image and animation and another part so that we can visualize the animation play and execution controls.

--- index.php
+++ index.php
@@ -3,8 +3,26 @@
 <head>
 	<meta charset="utf-8">
 	<title>Animator Image</title>
+	<link rel="stylesheet" type="text/css" href="app/css/style.css">
 </head>
 <body>
 
+	<div class="painel">
+		<div class="painel-animator">
+			<img src="" class="painel-image-display">
+			<div class="painel-group">
+				<div class="painel-group-item">
+					<!--Add item-->
+				</div>
+				<div class="painel-group-item">
+					<img src="" class="painel-group-item-image">
+				</div>
+				
+			</div>
+		</div>
+	</div>	
+
+	<script type="text/javascript" src="app/js/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="app/js/script.js"></script>
 </body>
 </html>
