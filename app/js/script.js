1991b46c25ad8500d86cce886731a225d8134e55
Cezar Leigos <cezarleigos28@outlook.com>
2021-09-04 19:29:14 -0300

    v0.03 - Creating actions
    This time we started to create the functions that will contain the actions that will be executed inside our application, soon we will implement each one of these functions.

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
 
-   
+	var frames = 0;
+	var frames_position = 0;
+	var frames_time = 24;
+
+    function animate_play(){
+
+    }
+
+    function animate_stop(){
+
+    }
+
+    function animate_pause(){
+
+    }
+
+    function edite_image(){
+
+    }
+
+    function close_image(){
+
+    }
+
+    function load_image(){
+
+    }
 
 });
