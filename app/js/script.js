ed2c2eb8e5d2d54c90300176080efdd5c4847e2e
Cezar Leigos <cezarleigos28@outlook.com>
2021-09-05 21:40:14 -0300

    v0.04 - Creating images
    This time we created some images that we can use as an example to run our animation, we also created some more functions and created a timer to set our animation.

--- app/js/script.js
+++ app/js/script.js
@@ -2,30 +2,53 @@ $(document).ready(function(){
 
 	var frames = 0;
 	var frames_position = 0;
-	var frames_time = 24;
+	var frames_time = 700;
+	var state = "stop";
 
-    function animate_play(){
+	function animate_play(){
 
-    }
+	}
 
-    function animate_stop(){
+	function animate_stop(){
 
-    }
+	}
 
-    function animate_pause(){
+	function animate_pause(){
 
-    }
+	}
 
-    function edite_image(){
+	function edite_image(){
 
-    }
+	}
 
-    function close_image(){
+	function close_image(){
 
-    }
+	}
 
-    function load_image(){
+	function load_image(){
 
-    }
+	}
+
+	function animate_execute(){
+
+		if(state == "play"){
+
+			animate_play();
+
+		}else if(state == "pause"){
+
+			animate_pause();
+
+		}else if(state == "stop"){
+
+			animate_stop();
+			
+		}else{
+
+		}
+
+	}
+
+	setInterval(animate_execute, frames_time);
 
 });
